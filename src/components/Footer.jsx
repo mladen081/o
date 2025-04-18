ecfc756a8b035e792000976ec4ddfe10a69e179f
mladen081 <jovicmladen094@gmail.com>
2025-04-18 22:46:04 +0200

    003 up++

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} Budget App. All rights reserved.</p>
+      <p>© {new Date().getFullYear()} Pumpaj.</p>
     </footer>
   );
 };
